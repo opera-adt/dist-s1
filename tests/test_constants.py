0e5a335b3757a312db537c6eb47ceb8c9042d348
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2025-08-01 09:59:10 -0700

    remove extra constant

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -1,9 +1,8 @@
-from dist_s1.constants import DISTVAL2LABEL, TIF_LAYERS, TIF_LAYER_DTYPES, TIF_LAYER_NODATA_VALUES
+from dist_s1.constants import TIF_LAYERS, TIF_LAYER_DTYPES, TIF_LAYER_NODATA_VALUES
 
 
 def test_tif_layer_nodata_values() -> None:
-    keys_nodata_values = set(TIF_LAYER_NODATA_VALUES.keys())
-    keys_dtypes = set(TIF_LAYER_DTYPES.keys())
-    label_keys = set(DISTVAL2LABEL.keys())
+    layer_nodata_vals = set(TIF_LAYER_NODATA_VALUES.keys())
+    layer_dtypes = set(TIF_LAYER_DTYPES.keys())
     layers = set(TIF_LAYERS)
-    assert keys_nodata_values == keys_dtypes == label_keys == layers
+    assert layer_nodata_vals == layer_dtypes == layers
