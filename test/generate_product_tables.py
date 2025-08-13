b503e77aa965ebf20e74fae2908d6af16f786699
cmarshak <cmarshak@users.noreply.github.com>
2025-08-13 17:52:40 +0000

    deploy: 1c14fff88e72f67e6fe83950bc109c561b0919fc

--- test/generate_product_tables.py
+++ test/generate_product_tables.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python3
-import sys
 from pathlib import Path
-
-
-sys.path.insert(0, str(Path(__file__).parent.parent / 'src'))
 
 from dist_s1.data_models.output_models import ProductNameData
 
